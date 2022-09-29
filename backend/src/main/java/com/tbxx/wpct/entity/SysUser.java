053c88bd8ca1bdf41cf420ca74a6d5e72b154463
zhuo <zhuo@qq.com>
2022-09-29 18:56:37 +0800

    Initial commit

--- backend/src/main/java/com/tbxx/wpct/entity/SysUser.java
+++ backend/src/main/java/com/tbxx/wpct/entity/SysUser.java
@@ -2,10 +2,10 @@ package com.tbxx.wpct.entity;
 
 /**
  * @author ZXX
- * @ClassName SysUse
+ * @ClassName SysUser
  * @Description TODO
  * @DATE 2022/9/29 18:37
  */
 
-public class SysUse {
+public class SysUser {
 }
