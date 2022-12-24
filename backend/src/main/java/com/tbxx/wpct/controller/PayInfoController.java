9add7f85b75d7484728dfc364193ee77b10129f0
aykls <1781954235@qq.com>
2022-12-24 13:45:42 +0800

    变更

---
+++ backend/src/main/java/com/tbxx/wpct/controller/PayInfoController.java
@@ -0,0 +1,29 @@
+package com.tbxx.wpct.controller;
+
+import com.tbxx.wpct.dto.PayInfoVo;
+import com.tbxx.wpct.dto.Result;
+import com.tbxx.wpct.entity.PayInfo;
+import com.tbxx.wpct.service.PayInfoService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@CrossOrigin
+@Api(tags = "缴费管理")
+@Slf4j
+@RestController
+@RequestMapping("/payInfo")
+public class PayInfoController {
+    @Autowired
+    PayInfoService payInfoService;
+    @RequiresPermissions("payInfo:list")
+    @ApiOperation("信息列表")
+    @GetMapping("/list")
+    public List<PayInfo> selectCondition(PayInfoVo vo) {
+        return payInfoService.selectCondition(vo);
+    }
+}
